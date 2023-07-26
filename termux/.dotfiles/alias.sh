8a1879aa3d5ba8630598fc2c91fd510b0b387b6d
Phate Valleyman <Jonas.Ned@outlook.com>
2023-07-27 01:38:50 +0200

    modified:   termux/.dotfiles/alias.sh 	renamed:    termux/.dotfiles/dialogrc -> termux/rcfiles/dialogrc 	renamed:    termux/.dotfiles/gitconfig -> termux/rcfiles/gitconfig 	renamed:    termux/.dotfiles/inputrc -> termux/rcfiles/inputrc 	renamed:    termux/.dotfiles/nanorc -> termux/rcfiles/nanorc

--- termux/.dotfiles/alias.sh
+++ termux/.dotfiles/alias.sh
@@ -12,11 +12,19 @@ alias rm='yes | rm -r -v'
 alias cp='cp -v -r'
 alias mv='mv -f -f'
 
+alias 'ts'='telnet 192.168.1.20'
+
 alias 'tree'='tree -AC --dirsfirst'
+
+alias 'wol'='wol FC:F5:28:31:08:B1'
 
 alias 'pygmentize'='pygmentize -g'
 
 alias wget='wget --show-progress --continue'
+alias 'curl'='curl --progress-bar'
+
 alias reload='source /data/data/com.termux/files/usr/etc/profile'
 
 alias ssh-server='su -c BIND_INTERFACE=wlan0 DNS_OVERRIDE_IP=8.8.8.8 LD_PRELOAD=/system/lib/bindToInterface.so /data/adb/modules/ssh/usr/bin/ssh -i ~/.ssh/server root@192.168.1.20'
+
+alias rclone-web='rclone rcd --rc-web-gui --rc-user=root --rc-pass=24nascr'
